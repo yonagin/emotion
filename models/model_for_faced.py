4a553bc77f882b3a3b9d0b6466fc7780c70904b2
yonagin <3118391854@qq.com>
2026-04-25 20:43:13 +0800

    init

--- models/model_for_faced.py
+++ models/model_for_faced.py
@@ -29,12 +29,12 @@ class Model(nn.Module):
         elif param.classifier == 'all_patch_reps_onelayer':
             self.classifier = nn.Sequential(
                 Rearrange('b c s d -> b (c s d)'),
-                nn.Linear(32 * 10 * 200, param.num_of_classes),
+                nn.LazyLinear(param.num_of_classes),
             )
         elif param.classifier == 'all_patch_reps_twolayer':
             self.classifier = nn.Sequential(
                 Rearrange('b c s d -> b (c s d)'),
-                nn.Linear(32 * 10 * 200, 200),
+                nn.LazyLinear(10 * 200),
                 nn.ELU(),
                 nn.Dropout(param.dropout),
                 nn.Linear(200, param.num_of_classes),
@@ -42,7 +42,7 @@ class Model(nn.Module):
         elif param.classifier == 'all_patch_reps':
             self.classifier = nn.Sequential(
                 Rearrange('b c s d -> b (c s d)'),
-                nn.Linear(32 * 10 * 200, 10 * 200),
+                nn.LazyLinear(10 * 200),
                 nn.ELU(),
                 nn.Dropout(param.dropout),
                 nn.Linear(10 * 200, 200),
